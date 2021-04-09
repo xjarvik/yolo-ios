0f0cb18af46d79edcf6a61d5d79b2a317d427294
xjarvik <sowi18oc@student.ju.se>
2021-04-09 15:21:57 +0200

    Initial Commit

---
+++ yolo-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yolo-ios
+//
+//  Created by William Söder on 2021-04-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
